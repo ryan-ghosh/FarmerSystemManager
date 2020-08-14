7e1ac96b9a6c922eb21c1b6a049ad524dcbe994e
ryan-ghosh <66140294+ryan-ghosh@users.noreply.github.com>
2020-08-14 15:39:10 -0400

    Add files via upload

---
+++ FarmingSystemManager/ReqClasses.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+#define pb push_back
+
+using namespace std;
+
+class Inventory{
+public:
+    unordered_map<string,int> inventory;
+
+    void addItem(string item, int quantity){
+        if (inventory.find(item)==inventory.end()){
+            inventory[item] = quantity;
+        }else{
+            inventory[item] += quantity;
+        }
+    }
+
+    void reduceItem(string item, int quantity){
+        if (quantity <= inventory[item]){
+            inventory[item] -= quantity;
+        }else{
+            cout << "There is only " << inventory[item] << "in stock";
+        }
+    }
+
+    void removeItem(string item){
+        inventory.erase(item);
+    }
+
+    void showInventory(){
+        for(pair<string, int> i : inventory){
+            cout << i.first << ": " << i.second<<endl;
+        }
+    }
+
+};
+
+class PayRollSystem{
+public:
+    int money;
+    vector<Employee> employees;
+
+    PayRollSystem(int bank){
+        money = bank;
+    }
+
+    void payday(){
+        for(Employee i : employees){
+            money -= i.salary + i.deductions;
+        }
+    }
+};
+
+class Employee{
+public:
+    string name;
+    int salary;
+    int sickdays;
+    int deductions; //tax, late, missed over sick days
+
+    Employee(string a, int s){
+        name = a;
+        salary = s;
+    }
+};
+
+int main(){
+    Inventory inventory;
+    inventory.addItem("apples", 10);
+    inventory.showInventory();
+    inventory.addItem("bananas", 30);
+    inventory.addItem("cheetos", 500);
+    inventory.addItem("fuck", 0);
+    inventory.showInventory();
+    return 0;
+}
