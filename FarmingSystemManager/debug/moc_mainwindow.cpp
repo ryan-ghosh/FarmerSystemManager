77c18fc297fb76920ed01a02bf6cf4956594498c
solimantwali <65260618+solimantwali@users.noreply.github.com>
2020-08-15 21:24:02 -0400

    added full functionality to buttons for task manager. pain

--- FarmingSystemManager/debug/moc_mainwindow.cpp
+++ FarmingSystemManager/debug/moc_mainwindow.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[7];
-    char stringdata0[127];
+    QByteArrayData data[8];
+    char stringdata0[151];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -38,12 +38,14 @@ QT_MOC_LITERAL(2, 34, 0), // ""
 QT_MOC_LITERAL(3, 35, 20), // "on_invButton_clicked"
 QT_MOC_LITERAL(4, 56, 22), // "on_payButton_3_clicked"
 QT_MOC_LITERAL(5, 79, 23), // "on_urgentButton_clicked"
-QT_MOC_LITERAL(6, 103, 23) // "on_PayEmpButton_clicked"
+QT_MOC_LITERAL(6, 103, 23), // "on_PayEmpButton_clicked"
+QT_MOC_LITERAL(7, 127, 23) // "on_pushButton_2_clicked"
 
     },
     "MainWindow\0on_clearButton_clicked\0\0"
     "on_invButton_clicked\0on_payButton_3_clicked\0"
-    "on_urgentButton_clicked\0on_PayEmpButton_clicked"
+    "on_urgentButton_clicked\0on_PayEmpButton_clicked\0"
+    "on_pushButton_2_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -53,7 +55,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       6,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -61,13 +63,15 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   39,    2, 0x08 /* Private */,
-       3,    0,   40,    2, 0x08 /* Private */,
-       4,    0,   41,    2, 0x08 /* Private */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
+       1,    0,   44,    2, 0x08 /* Private */,
+       3,    0,   45,    2, 0x08 /* Private */,
+       4,    0,   46,    2, 0x08 /* Private */,
+       5,    0,   47,    2, 0x08 /* Private */,
+       6,    0,   48,    2, 0x08 /* Private */,
+       7,    0,   49,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -88,6 +92,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 2: _t->on_payButton_3_clicked(); break;
         case 3: _t->on_urgentButton_clicked(); break;
         case 4: _t->on_PayEmpButton_clicked(); break;
+        case 5: _t->on_pushButton_2_clicked(); break;
         default: ;
         }
     }
@@ -123,13 +128,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 6)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 6;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
+        if (_id < 6)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        _id -= 6;
     }
     return _id;
 }
